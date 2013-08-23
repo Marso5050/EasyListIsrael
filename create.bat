3567ba5a3ce16988b76df0905e6d9b7965d00e8a
rafiw <rafiw@mellanox.com>
2013-08-23 15:31:15 +0300

    added create to main folder and moved tools to main folder

---
+++ create.bat
@@ -0,0 +1,9 @@
+cd tools
+python addChecksum.py < IsraelList.txt > IsraelList2.txt
+python validateChecksum.py < IsraelList2.txt
+IF ERRORLEVEL 1 GOTO errorHandling
+move /y IsraelList2.txt IsraelList.txt
+exit
+:errorHandling
+echo Somthing went wrong
+pause
