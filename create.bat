fa8e501e7bfe1569a64da4099962d3f9b29bc292
rafiw <rafiw@mellanox.com>
2013-08-23 15:58:07 +0300

    Revert "added create to main folder and moved tools to main folder"
    This reverts commit 3567ba5a3ce16988b76df0905e6d9b7965d00e8a.

--- create.bat
+++
@@ -1,9 +0,0 @@
-cd tools
-python addChecksum.py < IsraelList.txt > IsraelList2.txt
-python validateChecksum.py < IsraelList2.txt
-IF ERRORLEVEL 1 GOTO errorHandling
-move /y IsraelList2.txt IsraelList.txt
-exit
-:errorHandling
-echo Somthing went wrong
-pause
